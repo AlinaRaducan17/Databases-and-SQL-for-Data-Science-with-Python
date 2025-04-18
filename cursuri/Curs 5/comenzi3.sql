628b79f0ac9f2c5b2d0158ef3c1f18ada016209a
Alina Serban <alina.decembrie@gmail.com>
2025-04-18 09:09:50 +0300

    mic update comment

--- cursuri/Curs 5/comenzi3.sql
+++ cursuri/Curs 5/comenzi3.sql
@@ -4,7 +4,7 @@ SELECT COUNT(*) AS "TOTAL" FROM profesor;
 SELECT * FROM profesor ORDER BY nume;
 SELECT * FROM profesor ORDER BY nume DESC;
 SELECT * FROM profesor ORDER BY nume DESC LIMIT 3;
-SELECT * FROM profesor ORDER BY nume DESC LIMIT 3 , 2;
+SELECT * FROM profesor ORDER BY nume DESC LIMIT 3 , 2;#sare peste primele 3 si afiseaza urmatoarele 2
 SELECT CONCAT(nume, ' ' ,prenume) FROM profesor;
 SELECT CONCAT(nume, ' ' ,prenume) AS "Nume complet" FROM profesor;
 SELECT MAX(nume) FROM profesor;
